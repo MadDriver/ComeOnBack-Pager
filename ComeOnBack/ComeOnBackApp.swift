f02d76b04e3949dd2cca9942c59f8e385da9880b
MadDriver <calvin.shultz@gmail.com>
2023-03-09 13:51:46 -0700

    Initial Commit

---
+++ ComeOnBack/ComeOnBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComeOnBackApp.swift
+//  ComeOnBack
+//
+//  Created by Calvin Shultz on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ComeOnBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
