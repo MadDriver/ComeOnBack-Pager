59a5ceb5758396285f17fb4bf2629fb82b3a05c5
Luke <git@lunoho.com>
2023-06-21 16:09:57 -0600

    Moved signin button to topleft

--- ComeOnBack/Home.swift
+++ ComeOnBack/Home.swift
@@ -13,80 +13,71 @@ struct Home: View {
     @State var signInViewIsActive = false
     
     var body: some View {
-        
-        VStack {
-            HStack {
-                Text("\(pagingVM.timeString(date: pagingVM.date))")
-                    .font(.system(size: 32, weight: .bold))
-                    .onAppear {
-                        let _ = pagingVM.updateTimer
-                    }
-                
-                Image(systemName: "pencil")
-                    .frame(width: 50, height: 50)
-                    .onTapGesture {
-                        if pagingVM.timeType == .standard {
-                            pagingVM.timeType = .military
-                        } else {
-                            pagingVM.timeType = .standard
+        ZStack(alignment: .topLeading) {
+            VStack {
+                HStack {
+                    Text("\(pagingVM.timeString(date: pagingVM.date))")
+                        .font(.system(size: 32, weight: .bold))
+                        .onAppear {
+                            let _ = pagingVM.updateTimer
                         }
-                    }
-            }
-            
-            
-            NavigationStack {
-                
-                VStack {
                     
-                    HStack {
-                        
-                        List {
-                            ForEach(pagingVM.onPosition) { controller in
-                                OnPositionCellView(controller: controller)
+                    Image(systemName: "pencil")
+                        .frame(width: 50, height: 50)
+                        .onTapGesture {
+                            if pagingVM.timeType == .standard {
+                                pagingVM.timeType = .military
+                            } else {
+                                pagingVM.timeType = .standard
                             }
                         }
-                        
-                        List {
-                            ForEach($pagingVM.onBreakControllers) { $controller in
-                                
-                                NavigationLink {
-                                    PagingView(controller: $controller)
-                                } label: {
-                                    StripView(controller: controller)
+                } // H Stack
+                
+                NavigationStack {
+                    VStack {
+                        HStack {
+                            List {
+                                ForEach(pagingVM.onPosition) { controller in
+                                    OnPositionCellView(controller: controller)
                                 }
-
+                            }
+                            
+                            List {
+                                ForEach($pagingVM.onBreakControllers) { $controller in
+                                    
+                                    NavigationLink {
+                                        PagingView(controller: $controller)
+                                    } label: {
+                                        StripView(controller: controller)
+                                    }
+                                    
+                                }
                             }
                         }
                     }
-                }
-                
-                
-                
-            }
+                } // Nav Stack
+            } // V Stack
             
-            HStack {
-                Button("SIGN IN", action: signInController)
-                    .buttonStyle(.borderedProminent)
-            }
+            Button("SIGN IN/OUT", action: signInController)
+                .buttonStyle(.borderedProminent)
+                .padding()
             
-        }
+        } // Z Stack
         .environmentObject(pagingVM)
         .sheet(isPresented: $signInViewIsActive) {
             Text("DFADFDSAFSFS")
         }
-    }
+    }// body
     
     func signInController() {
         signInViewIsActive = true
-        
     }
-    
 }
-
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
             .previewInterfaceOrientation(.landscapeLeft)
+            .previewDevice(PreviewDevice(rawValue: "iPad (10th generation)"))
     }
 }
