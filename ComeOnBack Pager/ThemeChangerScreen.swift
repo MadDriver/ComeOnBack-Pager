07a39d2c465ddf2eedefc744d85792e22ae95443
Dean <calvin.shultz@gmail.com>
2024-12-28 11:29:44 -0700

    forgot to add to comit

--- ComeOnBack Pager/ThemeChangerScreen.swift
+++ ComeOnBack Pager/ThemeChangerScreen.swift
@@ -7,12 +7,84 @@
 
 import SwiftUI
 
-struct ThemeChanger: View {
+struct ThemeChangerScreen: View {
+    
+    @Environment(\.colorScheme) private var scheme
+    @AppStorage("user_theme") private var userTheme: Theme = .systemDefault
+    @Namespace private var animation
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            
+            Text("Choose a Style")
+                .font(.title2.bold())
+                .padding(.top, 25)
+                .foregroundStyle(Color.black)
+                .bold()
+            
+            HStack(spacing: 0) {
+                ForEach(Theme.allCases, id: \.rawValue) { theme in
+                    Text(theme.rawValue)
+                        .padding(.vertical, 10)
+                        .frame(width: 100)
+                        .foregroundStyle(Color.black)
+                        .background {
+                            ZStack {
+                                if userTheme == theme {
+                                    Capsule()
+                                        .fill(.white)
+                                        .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+                                }
+                            }
+                            .animation(.snappy, value: userTheme)
+                        }
+                        .contentShape(.rect)
+                        .onTapGesture {
+                            userTheme = theme
+                        }
+                }
+            }
+            .padding(3)
+            .background(.gray.opacity(0.2), in: .capsule)
+            .padding(.top, 20)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .frame(height: 500)
+        .background(Color.white)
+        .clipShape(.rect(cornerRadius: 30))
     }
 }
 
 #Preview {
-    ThemeChanger()
+    ThemeChangerScreen()
 }
+
+enum Theme: String, CaseIterable {
+    
+    case systemDefault = "Default"
+    case light = "Light"
+    case dark = "Dark"
+    
+    func color(_ scheme: ColorScheme) -> Color {
+        switch self {
+        case .systemDefault:
+            return scheme == .dark ? .red : .purple
+        case .light:
+            return .purple
+        case .dark:
+            return .red
+        }
+    }
+    
+    var colorScheme: ColorScheme? {
+        switch self {
+        case .systemDefault:
+            return nil
+        case .light:
+            return .light
+        case .dark:
+            return .dark
+        }
+    }
+}
+
